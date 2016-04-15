689b540c17ff69e8b57c6e180fd5e1635e05c989
Jon Deluz <jondeluz@hotmail.com>
2016-04-15 02:03:08 -0700

    Added deleteInvoiceHeaderModal and delete functionality(on delete  status 500)

---
+++ src/DiyCmWeb/wwwroot/js/controllers/invoicesController.js
@@ -0,0 +1,21 @@
+app.controller('invoicesController', ['$scope', '$http', 'InvoicesService', function ($scope, $http, InvoicesService) {
+
+    $scope.message = 'Everyone come and look!';
+
+    var onError = function (reason) {
+        console.log(reason);
+    };
+    var onSuccess = function (data){
+      window.location.reload();
+      console.log(data);
+    };
+
+
+    $scope.deleteInvoiceHeader = function (id) {
+      var InvoiceHeaderId = id;
+      console.log(InvoiceHeaderId);
+      InvoicesService.deleteInvoiceHeader(InvoiceHeaderId)
+        .then(onSuccess, onError);
+    };
+
+}]);
